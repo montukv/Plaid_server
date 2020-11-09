f9ddd49d50b6cb452ee852916663805a4934dad8
montukv <46618399+montukv@users.noreply.github.com>
2020-11-09 13:07:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,23 +11,12 @@ from flask import jsonify
 
 app = Flask(__name__)
 
+PLAID_CLIENT_ID='5fa3b51c005141001378227a'
+PLAID_SECRET='e714f54ef346c824000d051972bd04'
+PLAID_PRODUCTS='transactions'
+PLAID_COUNTRY_CODES='US'
+PLAID_ENV='sandbox'
 
-# Fill in your Plaid API keys - https://dashboard.plaid.com/account/keys
-PLAID_CLIENT_ID = '5fa3b51c005141001378227a'
-PLAID_SECRET = 'e714f54ef346c824000d051972bd04'
-# Use 'sandbox' to test with Plaid's Sandbox environment (username: user_good,
-# password: pass_good)
-# Use `development` to test with live users and credentials and `production`
-# to go live
-PLAID_ENV = os.getenv('PLAID_ENV', 'sandbox')
-# PLAID_PRODUCTS is a comma-separated list of products to use when initializing
-# Link. Note that this list must contain 'assets' in order for the app to be
-# able to create and retrieve asset reports.
-PLAID_PRODUCTS = os.getenv('PLAID_PRODUCTS', 'transactions').split(',')
-
-# PLAID_COUNTRY_CODES is a comma-separated list of countries for which users
-# will be able to select institutions from.
-PLAID_COUNTRY_CODES = os.getenv('PLAID_COUNTRY_CODES', 'US').split(',')
 
 
 def empty_to_none(field):
